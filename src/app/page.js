daaa5e68866d069ff13ec72478d0872f7ac4187a
nessipenko <101459146+nessipenko@users.noreply.github.com>
2024-03-11 11:19:52 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Loading from "./loading";
 
 export default function Page() {
     const router = useRouter()
@@ -9,4 +10,5 @@ export default function Page() {
         router.push('/auto')
     }, [])
 
+    return (< Loading />)
 }
