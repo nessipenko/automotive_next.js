4c3b3a7bbc26e281c5b4a3d78184b90de92a32f1
nessipenko <101459146+nessipenko@users.noreply.github.com>
2024-03-11 08:29:13 +0100

    initial commit

---
+++ src/app/auto/loading.jsx
@@ -0,0 +1,15 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import PageContainer from '../components/pageContainer';
+
+const Loading = () => {
+    return (
+        <PageContainer>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress color="primary" variant="indeterminate" />
+            </Box>
+        </PageContainer>
+    );
+}
+
+export default Loading;
